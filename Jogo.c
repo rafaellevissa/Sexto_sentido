eff8af913d67800070b4ae2d6908af1d62be6d78
mariofelix13 <mariofelix@hotmail.com>
2016-03-18 20:16:38 -0300

    Update Jogo.c

--- Jogo.c
+++ Jogo.c
@@ -4,7 +4,18 @@
 
 int main()
 {
-printf("uma história começou...");
+printf("SEXTO SENTIDO
+
+Laura, mulher pouco comum, em relação ao tipo que estamos acostumados. Ingressou na faculdade de administração aos 16 anos, no segundo ano de faculdade já era presidente de uma empresa júnior de importação e exportação de produtos baratos produzido no Oriente. Na realidade Laura introduziu o conceito da loja R$ 1,99. Aos 20 anos   resolveu sair da casa dos seus pais, queria investir seu talento e conhecimento se aventurando numa grande Metrópole, foi então quando se mudou para São Paulo, cidade grande com milhões de habitantes.  Em Sampa fez seu MBA em In Comerc e energia sustentável. No seu primeiro emprego como trainee conheceu Rita, uma secretária executiva, que passou a ser sua melhor amiga.
+As 25 anos, Laura resolve se mudar para uma pequena cidade turística, conhecida pelas suas maravilhosas estancias, a cidade de Xamy, localizada no Nordeste brasileiro e muito visitada no verão. Ela queria investir em um produto genuinamente novo, o vento.  Ela sentia que aquele seria um bom momento e o fez.  Montou um parque de energia eólica.
+Dez anos depois Laura já era presidente de uma companhia de médio porte com mais de duzentos empregados, 60% dos moradores de Xamy eram seus empregados. Dona do maior parque eólico da região. Assim passou a ser conhecida como “A garota dos ventos”, e aos 36 anos já é uma milionária.
+Com todo esse sucesso, ela ainda se sente frustrada, gostaria de engravidar antes dos 40 anos. Apesar de ser uma grande empreendedora, Laura não consegue ter o mesmo desempenho quando o assunto é relacionamento pessoal. Está solteira e nunca teve relacionamentos duradouros. Laura sempre atribui seu fracasso ao fato de se dedicar inteiramente ao trabalho. Mas isso não é tudo. Ele teme ser enganada por algum homem, se aproveitando de sua condição financeira. Fato passível de ocorrer. 
+Rita, 52 anos, atualmente divorciada.  Conheceu seu ex-marido no jardim de infância. Tiveram um casamento que durou 30 anos, deste relacionamento tiveram uma filha. Por ter vivido um único relacionamento, não tem segurança para aconselhar as suas amigas, mas é uma boa ouvinte. Praticou artes maciais durante 05 anos. Com o seu divórcio, foi convidada por Laura para trabalhar na sua empresa, quando passou a dividir o apartamento com Maria.
+Maria, embora nunca tenha saído da cidade onde nasceu, apresenta características nada típica daquele lugar, formada em Teatro, é uma excelente atriz, mas sua profissão não lhe garantiu grandes benefícios, uma vez que suas oportunidades foram bastantes parcas. Seu gosto por baladas, a mantem pouco distante do tipo de homem que gosta de se relacionar, pois os mesmos normalmente estão na faixa dos 50 anos. De todas as suas amigas, ela costuma se irritar com Ana Lucia, que sempre faz questão de lembrá-la que vive em uma vida de sonhos.
+Considerada a primeira dama da cidade, por ser herdeira de umas das famílias tradicionais de Xamy, Ana Lucia, esbanja sua riqueza de todas as formas possíveis. Muito carinhosa, essa sua característica a faz ser a mais próxima de todos as amigas, que embora não possam compartilhar ou ostentar dos mesmos privilégios e gostos, amam a sua presença. Tem um namorado e um filho, órfã de pai, deseja que sua amiga Carolina seja madrinha de seu filho. Carol como é conhecida, rejeita tal ideia.
+Principal amiga de Ana Lucia, Carolina ama tecnologia, foi a principal articuladora da implantação tecnológica do Parque Eólico, gerido pela empresa de Laura. Tem 42 anos, cresceu no único orfanato da cidade, costuma afirmar que não gosta de crianças, mesmo diante da insistência de Ana Lucia para que batize o seu filho. Costuma se relacionar com rapazes mais jovens no máximo 24 anos. Seu culto ao corpo, ultrapassa o limite da razão, pratica musculação todos os dias da semana e sempre imagina acima do peso ideal.
+
+");
 
 return 0;
 }
